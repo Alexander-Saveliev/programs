f866de1f91e0a856cbd8fed7e0665a0bc2d70f12
Marty <marty@MacBook-Pro-Marty.local>
2017-05-21 19:14:32 +0300

    prog2: Changed the formatting, 'case' replaced with 'if'

--- 2/main.js
+++ 2/main.js
@@ -1,40 +1,28 @@
 "use strict";
 
 
-// const
-var Admin = "admin",
-    Admin_password = "Black Overlord",
-    Canceled = "Sign-in canceled",
-// var
-    Login = prompt("Login", "Enter login"),
-    Password;
+var ADMIN           = "admin",
+    ADMINPASSWORD   = "Black Overlord",
+    CANCELED        = "Sign-in canceled";
+
+var Login = prompt("Login", "Enter login");
 
 
+if ( Login == ADMIN ) {
 
-switch(Login) {
-    case Admin:
-        Password = prompt("Password", "Enter Password");
+        var Password = prompt("Password", "Enter Password");
 
-        switch(Password) {
-            case Admin_password:
-                alert("Welcome!");
-                break;
-
-            case null:
-                alert(Canceled);
-                break;
-
-            default:
-                alert("Password is incorrect");
+        if ( Password == ADMINPASSWORD ) {
+            alert("Welcome!");
+        } else
+        if ( Password == null ) {
+            alert(CANCELED);
+        } else {
+            alert("Password is incorrect");
         }
-        break;
 
-
-    case null:
-        alert(Canceled);
-        break;
-
-
-    default:
-        alert("I don't know you");
+} else if ( Login == null ) {
+    alert(CANCELED);
+} else {
+    alert("I don't know you");
 }
